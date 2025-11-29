ceec46a9ad18a67c6d8a139ac398a9077015d7f4
Ola <mirreal@gmail.com>
2025-11-29 21:49:02 +0100

    initialize Spring Boot project with basic structure and dependencies

---
+++ src/main/java/com/bigos/PhotoAlbum.java
@@ -0,0 +1,11 @@
+package com.bigos;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAlbum {
+    static void main(String[] args) {
+
+    }
+}
